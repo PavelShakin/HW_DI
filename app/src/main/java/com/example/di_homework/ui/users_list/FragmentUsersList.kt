3d016d537e9d86158f76f52d3ce0e1344e2588b2
Pavel Shakin <pashkin302@gmail.com>
2022-02-12 21:22:22 +0200

    Created BaseFragment and inherit it

--- app/src/main/java/com/example/di_homework/ui/users_list/FragmentUsersList.kt
+++ app/src/main/java/com/example/di_homework/ui/users_list/FragmentUsersList.kt
@@ -1,28 +1,16 @@
 package com.example.di_homework.ui.users_list
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.example.di_homework.R
 import com.example.di_homework.databinding.FragmentUsersListBinding
+import com.example.di_homework.ui.BaseFragment
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
-class FragmentUsersList : Fragment(R.layout.fragment_users_list) {
+class FragmentUsersList :
+    BaseFragment<FragmentUsersListBinding>(FragmentUsersListBinding::inflate) {
 
-    private lateinit var binding: FragmentUsersListBinding
     private val viewModel: UsersListViewModel by sharedViewModel()
     private val userAdapter = UsersListAdapter()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentUsersListBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
